accc0094f9ba38fc363905f17a75a839daee1e10
Oroitz Lago Ramos <oroitzlr@gmail.com>
2026-03-21 00:09:32 +0100

    feat creation of tank prefab and movement + testing

---
+++ UNITY/Assets/Scripts/Tanks/TankController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] private float _moveSpeed = 0.5f;
+    [SerializeField] private float _rotationSpeed = 180f;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _desiredPosition;
+    private float _desiredRotation;
+    #endregion
+
+    #region Testing Getters
+    public float MoveSpeed => _moveSpeed;
+    #endregion
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start(){}
+    private void Update(){}
+
+    void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _desiredPosition * _moveSpeed * Time.fixedDeltaTime);
+        //Quaternion rotation = Quaternion.LookRotation(_desiredPosition);
+        //_rigidbody.MoveRotation(rotation);
+    }
+    public void Move(Vector2 direction)
+    {
+        _desiredPosition = direction.normalized;
+    }
+
+    public void Rotate(float angle)
+    {
+        _desiredRotation = angle * _rotationSpeed;
+    }
+
+    public void Stop()
+    {
+        _desiredPosition = Vector2.zero;
+        _desiredRotation = 0;
+    }
+}
---
+++ UNITY/Assets/Scripts/Testing/SimulateTankMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulateTankMove : MonoBehaviour
+{
+    [SerializeField] private TankController _tankController;
+
+    private Vector2 _moveTo = new Vector2(10f, 0f);
+    private float _timeToReachDestination;
+    private float _timer;
+
+    private bool _status = true;
+    // Start is called before the first frame update
+    void Start()
+    {   _timeToReachDestination = CalculateTimeToReachDestination();
+        _tankController.Move(_moveTo);
+        _timer = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_status) { return; }
+
+        if (Vector2.Distance(_tankController.transform.position, _moveTo) < 0.1f)
+        {
+            _tankController.Stop();
+            Debug.Log("Tank reached the destination.");
+            _status = false;
+        }
+        if (Time.time - _timer > _timeToReachDestination + 1f)
+        {
+            Debug.LogWarning("Tank did not reach the destination within the expected time.");
+            _tankController.Stop();
+            _status = false;
+        }
+    }
+
+    private float CalculateTimeToReachDestination()
+    {
+        float distance = Vector2.Distance(_tankController.transform.position, _moveTo);
+        return distance / (_tankController.MoveSpeed * Time.fixedDeltaTime);
+    }
+}
