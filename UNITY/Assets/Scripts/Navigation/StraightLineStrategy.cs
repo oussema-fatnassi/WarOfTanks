5f084f2228591752116a25564be0c4da5f2e7b9a
Oroitz Lago Ramos <oroitzlr@gmail.com>
2026-04-20 14:33:25 +0200

    refactor: remove unused distanceThreshold field from StraightLineStrategy

--- UNITY/Assets/Scripts/Navigation/StraightLineStrategy.cs
+++ UNITY/Assets/Scripts/Navigation/StraightLineStrategy.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class StraightLineStrategy : NavigationStrategy
 {
-    [SerializeField] private float distanceThreshold = 0.1f;
-
     public override List<Vector2> ComputePath(Vector2 from, Vector2 to)
     {
         return new List<Vector2> { to };
