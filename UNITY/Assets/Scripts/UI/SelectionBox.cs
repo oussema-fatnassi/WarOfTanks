fcffe3793d346482da212f4be53d0635068812f7
Oroitz Lago Ramos <oroitzlr@gmail.com>
2026-04-22 13:00:21 +0200

    feat: add SelectionBox script for UI drag selection functionality

---
+++ UNITY/Assets/Scripts/UI/SelectionBox.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionBox : MonoBehaviour
+{
+    #region Fields
+    private RectTransform _rectTransform;
+    private UnityEngine.UI.Image _image;
+    private Vector2 _startPos;
+    private Vector2 _currentPos;
+    #endregion
+
+    #region Unity Methods
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
+        _image.enabled = false;
+    }
+
+    public void BeginDrag(Vector2 screenStart)
+    { 
+        _startPos = screenStart;
+        _currentPos = screenStart;
+        _image.enabled = true;
+    }
+
+    public void UpdateDrag(Vector2 screenCurrentPosition)
+    {
+        _currentPos = screenCurrentPosition;
+        _rectTransform.anchoredPosition = (_startPos + _currentPos) / 2f;
+        _rectTransform.sizeDelta = new Vector2(Mathf.Abs(_startPos.x - _currentPos.x), Mathf.Abs(_startPos.y - _currentPos.y));
+    }
+
+    public Rect EndDrag()
+    {
+        _image.enabled = false;
+        return new Rect(Mathf.Min(_startPos.x, _currentPos.x), Mathf.Min(_startPos.y, _currentPos.y), Mathf.Abs(_currentPos.x - _startPos.x), Mathf.Abs(_currentPos.y - _startPos.y));
+    }
+
+    #endregion
+}
