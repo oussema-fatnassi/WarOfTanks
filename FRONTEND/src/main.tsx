872dd0fc7afdc2f904715f7d36d728270978228b
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2026-06-18 21:31:23 +0200

    feat(styles): refactor CSS variables for improved theming and update font imports for better typography

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthProvider.tsx'
+import '@fontsource-variable/inter/index.css'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/500.css'
+import '@fontsource/jetbrains-mono/700.css'
 import './index.css'
 import App from './App.tsx'
 
